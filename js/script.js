2dc5a2625ca2ad84981a1ceb005c560b544ed10a
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-02-24 22:10:44 +0630

    add testimonial and early access sections

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const themeToggleButton = document.getElementById("theme-toggle");
+const themeToggleBtn = document.getElementById("theme-toggle");
 const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
 const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
@@ -7,23 +7,23 @@ if (
   (!("color-theme" in localStorage) &&
     window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
-  // Show Light Icon
+  // Show light icon
   themeToggleLightIcon.classList.remove("hidden");
 } else {
   themeToggleDarkIcon.classList.remove("hidden");
 }
 
 // Listen for toggle button click
-themeToggleButton.addEventListener("click", toggleMode);
+themeToggleBtn.addEventListener("click", toggleMode);
 
 function toggleMode() {
-  // Toggle Icon
+  // Toggle icon
   themeToggleDarkIcon.classList.toggle("hidden");
   themeToggleLightIcon.classList.toggle("hidden");
 
-  // If isset in localStorage
+  // If is set in localstorage
   if (localStorage.getItem("color-theme")) {
-    // If light, make dark and save in localStorage
+    // If light, make dark and save in localstorage
     if (localStorage.getItem("color-theme") === "light") {
       document.documentElement.classList.add("dark");
       localStorage.setItem("color-theme", "dark");
@@ -32,7 +32,7 @@ function toggleMode() {
       localStorage.setItem("color-theme", "light");
     }
   } else {
-    // If not in localStorage
+    // If not in localstorage
     if (document.documentElement.classList.contains("dark")) {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("color-theme", "light");
